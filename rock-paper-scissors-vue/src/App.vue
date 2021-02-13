63914f5833a79a8892a3659a45113c55d7caf468
Hugo Arru <hugo.arru44@gmail.com>
2021-02-13 12:22:57 +0100

    chore(rps): setup vue and scss

---
+++ rock-paper-scissors-vue/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+	<span>test</span>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ rock-paper-scissors-vue/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "@/styles/_main.scss";`
+      }
+    }
+  }
+};
+
